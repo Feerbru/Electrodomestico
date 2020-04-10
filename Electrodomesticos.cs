7ea1021b2eb9db803690c1cd462de2fba39584d7
Fernando Bruzzo <fernandobruzzo7@gmail.com>
2020-04-09 21:56:02 -0300

    Agregar archivos de proyecto.

---
+++ Electrodomesticos.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Electrodomestico
+{
+    public class Electrodomesticos
+    {
+        //Variables Constantes
+        const double precioBase = 100.00;
+        const string color = "Blanco";
+        const  char consumoEnergetico = 'F';
+        const int peso = 5;
+
+     
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Electrodomestico
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
